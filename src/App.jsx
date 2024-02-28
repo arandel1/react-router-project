414785f3dddd3c3e70adb1a4c5de07961cf19a46
arandel1 <allison.randel@gmail.com>
2024-02-28 14:03:20 -0600

    finishing up tab completed and added an orange componenet

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,29 @@ import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import Blue from './components/Blue';
 import Red from './components/Red';
+import Home from './components/Home';
+import Orange from './components/Orange';
 
 function App() {
-  
 
   return (
     <>
-    <div id="container">
-      <div id="navbar">
-        <Link to = '/'>Home</Link>
-        <Link to = '/'>Blue</Link>
-        <Link to = '/'>Red</Link>
+      <div id="container">
+        <div id="navbar">
+          <Link to = '/'>Home</Link>
+          <Link to = '/'>Red</Link>
+          <Link to = '/'>Orange</Link>
+          <Link to = '/'>Blue</Link>
+        </div>
+        <div id="main-section">
+          <Routes>
+            <Route path = '/blue' element = {<Blue/>} />
+            <Route path = '/red' element = {<Red/>} />
+            <Route path = '/orange' element = {<Orange/>} />
+            <Route path = '/home' element = {<Home/>} />
+          </Routes>
+        </div>
       </div>
-      <div id="main-section">
-        <Routes>
-          <Route path = '/blue' element = {<Blue/>} />
-          <Route path = '/red' element = {<Red/>} />
-        </Routes>
-      </div>
-    </div>
     </>
   )
 }
---
+++ src/components/Orange.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+
+function Orange(){
+
+  return (
+    <>
+      <div className = "orange">
+        <h1>ORANGE</h1>
+      </div>
+    </>
+  )
+}
+
+export default Orange
